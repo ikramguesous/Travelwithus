41ecf7b22c2720e32f8ec9de9c145f0fb3fb3a2b
mac <mac@192.168.1.94>
2024-11-29 15:02:45 +0100

    Message de commit explicite

--- connection.php
+++ connection.php
@@ -6,6 +6,7 @@ $host = "serveurmysql.mysql.database.azure.com"; // Nom d'hôte Azure
 $username = "Ikram_Guessous"; // Nom d'utilisateur
 $password = "Poisson2002"; // Mot de passe
 $dbname = "ma_base"; // Nom de la base de données
+$ca_cert_path = "DigiCertGlobalRootCA.crt.pem"; // Chemin du certificat SSL téléchargé
 
 // Activer l'affichage des erreurs pour le débogage
 ini_set('display_errors', 1);
@@ -13,7 +14,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 try {
-    // Connexion à la base de données avec gestion des erreurs
+    // Connexion à la base de données avec SSL
     $conn = new mysqli($host, $username, $password, $dbname);
 
     // Vérification de la connexion
@@ -21,9 +22,9 @@ try {
         throw new Exception("Erreur de connexion : " . $conn->connect_error);
     }
 
-    // Désactiver SSL
-    $conn->options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);  // Désactiver la vérification du certificat SSL
-    $conn->ssl_set(NULL, NULL, NULL, NULL, NULL);  // Désactiver la connexion SSL
+    // Configuration SSL
+    $conn->ssl_set(NULL, NULL, $ca_cert_path, NULL, NULL); // Utilisation du certificat CA
+    $conn->real_connect($host, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL);
 
 } catch (Exception $e) {
     die("Une erreur est survenue : " . $e->getMessage());
