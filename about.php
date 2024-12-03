9dad3518c2604254e07d48ddf6194f7ec1d836f9
mac <mac@MacBook-Air.local>
2024-12-03 15:52:39 +0100

    Message de commit explicite

--- about.php
+++ about.php
@@ -211,6 +211,7 @@
   <nav class="navbar">
     <a href="home.php">Home</a>
     <a href="About.php">About</a>
+    <a href="package.php">Package</a>
     <a href="Book.php">Book</a>
   </nav>
   <div id="menu-btn" class="fas fa-bars"></div>
